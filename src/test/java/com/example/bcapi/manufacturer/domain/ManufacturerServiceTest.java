eedfd1a477ca2a6f4e0975e2a61be4c9f7a75a23
Joe Martin <joemartin.martin@gmail.com>
2026-05-12 17:23:59 +0200

    Add error case & update test naming

--- src/test/java/com/example/bcapi/manufacturer/domain/ManufacturerServiceTest.java
+++ src/test/java/com/example/bcapi/manufacturer/domain/ManufacturerServiceTest.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import static java.time.OffsetDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -16,13 +17,23 @@ class ManufacturerServiceTest {
     private final ManufacturerService manufacturerService = new ManufacturerService(manufacturerRepository);
 
     @Test
-    void create_createsManufacturer() {
-        var manufacturerDraft = new ManufacturerDraft("Heineken", "NL");
-        var expected = new Manufacturer(UUID.randomUUID(), "Heineken", "NL", now(),  now());
+    void create_validDraft_returnsPersistedManufacturer() {
+        var draft = new ManufacturerDraft("Heineken", "NL");
+        var expected = new Manufacturer(UUID.randomUUID(), "Heineken", "NL", now(), now());
         when(manufacturerRepository.create(any())).thenReturn(expected);
 
-        Manufacturer result = manufacturerService.create(manufacturerDraft);
+        var result = manufacturerService.create(draft);
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void create_repositoryFails_throwsException() {
+        var draft = new ManufacturerDraft("Heineken", "NL");
+        when(manufacturerRepository.create(any())).thenThrow(new RuntimeException("persistence failure"));
+
+        assertThatThrownBy(() -> manufacturerService.create(draft))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("persistence failure");
     }
 }
